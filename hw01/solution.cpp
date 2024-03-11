7cd1eb9f0de17f90a7b106b4f351497f2e145364
Jan Kuchař <kuchaj19@fit.cvut.cz>
2024-03-11 09:43:07 +0100

    Reformat solution.cpp

--- hw01/solution.cpp
+++ hw01/solution.cpp
@@ -1,77 +1,73 @@
 #ifndef __PROGTEST__
+#include "progtest_solver.h"
+#include "sample_tester.h"
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cfloat>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <compare>
+#include <condition_variable>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <cstdint>
-#include <climits>
-#include <cfloat>
-#include <cassert>
-#include <cmath>
-#include <iostream>
+#include <deque>
+#include <functional>
 #include <iomanip>
-#include <algorithm>
-#include <numeric>
-#include <string>
-#include <vector>
-#include <array>
+#include <iostream>
 #include <iterator>
-#include <set>
 #include <list>
 #include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <compare>
-#include <queue>
-#include <stack>
-#include <deque>
 #include <memory>
-#include <functional>
-#include <thread>
 #include <mutex>
-#include <atomic>
-#include <chrono>
-#include <stdexcept>
-#include <condition_variable>
+#include <numeric>
 #include <pthread.h>
+#include <queue>
 #include <semaphore.h>
-#include "progtest_solver.h"
-#include "sample_tester.h"
+#include <set>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 #endif /* __PROGTEST__ */
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
-class COptimizer
-{
-  public:
-    static bool                        usingProgtestSolver                     ( void )
-    {
-      return true;
-    }
-    static void                        checkAlgorithmMin                       ( APolygon                              p )
-    {
-      // dummy implementation if usingProgtestSolver() returns true
-    }
-    static void                        checkAlgorithmCnt                       ( APolygon                              p )
-    {
-      // dummy implementation if usingProgtestSolver() returns true
-    }
-    void                               start                                   ( int                                   threadCount );
-    void                               stop                                    ( void );
-    void                               addCompany                              ( ACompany                              company );
-  private:
+class COptimizer {
+public:
+	static bool usingProgtestSolver(void) {
+		return true;
+	}
+	static void checkAlgorithmMin(APolygon p) {
+		// dummy implementation if usingProgtestSolver() returns true
+	}
+	static void checkAlgorithmCnt(APolygon p) {
+		// dummy implementation if usingProgtestSolver() returns true
+	}
+	void start(int threadCount);
+	void stop(void);
+	void addCompany(ACompany company);
+
+private:
 };
 // TODO: COptimizer implementation goes here
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 #ifndef __PROGTEST__
-int                                    main                                    ( void )
-{
-  COptimizer optimizer;
-  ACompanyTest  company = std::make_shared<CCompanyTest> ();
-  optimizer . addCompany ( company );
-  optimizer . start ( 4 );
-  optimizer . stop  ();
-  if ( ! company -> allProcessed () )
-    throw std::logic_error ( "(some) problems were not correctly processsed" );
-  return 0;
+int main(void) {
+	COptimizer optimizer;
+	ACompanyTest company = std::make_shared<CCompanyTest>();
+	optimizer.addCompany(company);
+	optimizer.start(4);
+	optimizer.stop();
+	if (!company->allProcessed())
+		throw std::logic_error("(some) problems were not correctly processsed");
+	return 0;
 }
 #endif /* __PROGTEST__ */
