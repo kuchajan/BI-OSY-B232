813ed9752efdf1923216f9daa6465ffd3808200b
Jan Kuchař <kuchaj19@fit.cvut.cz>
2024-03-11 09:36:50 +0100

    Upload files from ProgTest

---
+++ hw01/solution.cpp
@@ -0,0 +1,77 @@
+#ifndef __PROGTEST__
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <climits>
+#include <cfloat>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <set>
+#include <list>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <compare>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <memory>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <chrono>
+#include <stdexcept>
+#include <condition_variable>
+#include <pthread.h>
+#include <semaphore.h>
+#include "progtest_solver.h"
+#include "sample_tester.h"
+using namespace std;
+#endif /* __PROGTEST__ */
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+class COptimizer
+{
+  public:
+    static bool                        usingProgtestSolver                     ( void )
+    {
+      return true;
+    }
+    static void                        checkAlgorithmMin                       ( APolygon                              p )
+    {
+      // dummy implementation if usingProgtestSolver() returns true
+    }
+    static void                        checkAlgorithmCnt                       ( APolygon                              p )
+    {
+      // dummy implementation if usingProgtestSolver() returns true
+    }
+    void                               start                                   ( int                                   threadCount );
+    void                               stop                                    ( void );
+    void                               addCompany                              ( ACompany                              company );
+  private:
+};
+// TODO: COptimizer implementation goes here
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+#ifndef __PROGTEST__
+int                                    main                                    ( void )
+{
+  COptimizer optimizer;
+  ACompanyTest  company = std::make_shared<CCompanyTest> ();
+  optimizer . addCompany ( company );
+  optimizer . start ( 4 );
+  optimizer . stop  ();
+  if ( ! company -> allProcessed () )
+    throw std::logic_error ( "(some) problems were not correctly processsed" );
+  return 0;
+}
+#endif /* __PROGTEST__ */
